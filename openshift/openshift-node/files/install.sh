e9274df2e3193b67b2527a855e29939b31c426bb
Laurent Eschenauer <laurent@eschenauer.be>
2013-02-18 23:00:31 +0100

    Fixes for the node

--- openshift/openshift-node/files/install.sh
+++ openshift/openshift-node/files/install.sh
@@ -4,6 +4,10 @@ mkdir -p /var/log/comodit/openshift-node/
 
 (
 
+# Disable iptables
+/bin/systemctl stop iptables.service
+/bin/systemctl disable iptables.service
+
 # Patch to enable openshift-cgroups service to start
 /bin/patch -t -p1 /bin/oo-admin-ctl-cgroups < /var/lib/comodit/applications/openshift-node/systemd.patch
 
