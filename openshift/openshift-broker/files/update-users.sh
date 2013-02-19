11b55c8ee6d06f8e959ce6d3a353aa2f421fa93d
Laurent Eschenauer <laurent@eschenauer.be>
2013-02-19 22:38:04 +0100

    Fixed user management issue

--- openshift/openshift-broker/files/update-users.sh
+++ openshift/openshift-broker/files/update-users.sh
@@ -5,8 +5,8 @@
 touch /etc/openshift/htpasswd
 <#if users??>
   <#list users as user>
-htpasswd -b /etc/openshift/htpasswd ##user.username## ##user.password##
+htpasswd -b /etc/openshift/htpasswd ${user.username} ${user.password}
   </#list>
 </#if>
 
-) >> /var/log/comodot/openshift-broker/update-users.log 2>&1
+) >> /var/log/comodit/openshift-broker/update-users.log 2>&1
