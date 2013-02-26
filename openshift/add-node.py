a5fba890c99df01b5f6973fde08acbee088908f4
Laurent Eschenauer <laurent@eschenauer.be>
2013-02-26 20:32:08 +0100

    Revert to platform hostname if reverse did not work

--- openshift/add-node.py
+++ openshift/add-node.py
@@ -73,7 +73,11 @@ def scale():
     # Install openshift-node
     print "Installing Openshift Node"
     public_ip = node.get_instance().wait_for_property('publicIp', config.time_out)
-    public_hostname = socket.gethostbyaddr(public_ip)
+    try:
+        public_hostname = socket.gethostbyaddr(public_ip)
+    except:
+        public_hostname = node.get_instance().wait_for_address(config.time_out)
+        
     node.install("openshift-node", {"broker_host": "broker." + config.domain, 
                                     "public_hostname": public_hostname[0], 
                                     "public_ip": public_ip, 
